59a381a02f1851bdd292f0db4e1d23c9a080babd
pedromv753/lp2 <pedromv753@gmail.com>
2024-09-22 21:31:01 -0500

    practicas 01 ejercicio

---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_a/datos/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.patitas_frontend_a.datos;
+
+public record LoginRequestDTO(String tipoDocumento, String numeroDocumento, String password) {
+}
---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_a/web/AppWebConfig.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.patitas_frontend_a.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppWebConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
